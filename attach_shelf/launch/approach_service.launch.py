908450ea48e1194a01618b80298f0e076ecf955f
asmbatati <asmalbatati@hotmail.com>
2024-12-26 11:45:19 +0000

    Modified rviz2

---
+++ attach_shelf/launch/approach_service.launch.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    pkg_name = 'attach_shelf'
+    pkg_share_name = FindPackageShare(pkg_name)
+
+    config_dir_name = 'config'
+
+    # ========
+    # ARGUMENT
+    # ========
+    # use_sim_time argument (determines simulator vs real lab)
+    use_sim_time_f = LaunchConfiguration('use_sim_time')
+    use_sim_time_arg = DeclareLaunchArgument(
+            'use_sim_time', default_value=TextSubstitution(text='true'),
+            description='config for simulator (true) or lab (false)')
+
+    # ======
+    # CONFIG
+    # ======
+    # Rviz2 config file path argument
+    rviz_config_dir_name = 'rviz'
+
+    rviz_config_file_name_sim = 'config_sim.rviz'
+    rviz_config_dir_sim = PathJoinSubstitution([pkg_share_name, rviz_config_dir_name, rviz_config_file_name_sim])
+
+    rviz_config_file_name_lab = 'config_lab.rviz'
+    rviz_config_dir_lab = PathJoinSubstitution([pkg_share_name, rviz_config_dir_name, rviz_config_file_name_lab])
+
+    # ====
+    # NODE
+    # ====
+    # Rviz2 node (configured for sim or lab)
+    rviz_node_sim = Node(
+            package='rviz2', executable='rviz2', output='screen', name='rviz_node',
+            parameters=[{'use_sim_time': use_sim_time_f}],
+            arguments=['-d', rviz_config_dir_sim],
+            condition=IfCondition(use_sim_time_f))
+
+    rviz_node_lab = Node(
+            package='rviz2', executable='rviz2', output='screen', name='rviz_node',
+            parameters=[{'use_sim_time': use_sim_time_f}],
+            arguments=['-d', rviz_config_dir_lab],
+            condition=UnlessCondition(use_sim_time_f))
+
+    # ======
+    # CONFIG
+    # ======
+    # attach service server node configuration
+    attach_service_config_file_name_sim = 'approach_service_server_node_sim.yaml'
+    attach_service_config_path_sim = PathJoinSubstitution([pkg_share_name, config_dir_name, attach_service_config_file_name_sim])
+
+    attach_service_config_file_name_lab = 'approach_service_server_node_lab.yaml'
+    attach_service_config_path_lab = PathJoinSubstitution([pkg_share_name, config_dir_name, attach_service_config_file_name_lab])
+
+    # ====
+    # NODE
+    # ====
+    # `/approach_shelf` service server node
+    final_approach_node_sim = Node(
+        package='attach_shelf', executable='approach_service_server_node',
+        output='screen', name='approach_service_server_node', emulate_tty=True,
+        parameters=[{'use_sim_time': use_sim_time_f}, attach_service_config_path_sim],
+        remappings=[('/cmd_vel', '/diffbot_base_controller/cmd_vel_unstamped')],
+        condition=IfCondition(use_sim_time_f)
+    )
+
+    final_approach_node_lab = Node(
+        package='attach_shelf', executable='approach_service_server_node',
+        output='screen', name='approach_service_server_node', emulate_tty=True,
+        parameters=[{'use_sim_time': use_sim_time_f}, attach_service_config_path_lab],
+        condition=UnlessCondition(use_sim_time_f)
+    )
+
+    # ==================
+    # LAUNCH DESCRIPTION
+    # ==================
+    return LaunchDescription(
+        [
+            use_sim_time_arg,
+            rviz_node_sim,
+            rviz_node_lab,
+            final_approach_node_sim,
+            final_approach_node_lab
+        ]
+    )
---
+++ attach_shelf/launch/pre_approach_v2.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    obstacle_arg = DeclareLaunchArgument(
+        "obstacle", default_value="0.30"
+    )
+    degrees_arg = DeclareLaunchArgument(
+        "degrees", default_value="-90.0"
+    )
+    final_approach_arg = DeclareLaunchArgument(
+        "final_approach", default_value="true"
+    )
+
+    obstacle_f = LaunchConfiguration('obstacle')
+    degrees_f = LaunchConfiguration('degrees')
+    final_approach_f = LaunchConfiguration('final_approach')
+
+    pre_approach_node = Node(
+        package='attach_shelf',
+        executable='pre_approach_v2_node',
+        output='screen',
+        name='pre_approach_v2_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True}],
+        arguments=["-obstacle", obstacle_f,
+                   "-degrees", degrees_f,
+                   "-final_approach", final_approach_f,]
+    )
+
+    # create and return launch description object
+    return LaunchDescription(
+        [
+            obstacle_arg,
+            degrees_arg,
+            final_approach_arg,
+            pre_approach_node
+        ]
+    )
